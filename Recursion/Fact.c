5064fcfd5f025d05ff5cdcabac136299803ebe4f
Sherin Elsy Behanan <86961675+sherin-elsy-behanan@users.noreply.github.com>
2023-09-16 10:18:52 +0530

    Create Fact.c

---
+++ Recursion/Fact.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int fact(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    return n*fact(n-1);
+}
+int main()
+{
+    int num,f;
+    printf("Enter a number");
+    scanf("%d",&num);
+    f=fact(num);
+    printf("Factorial : %d",f);
+
+    return 0;
+}
