3dab54272d1a4d363cc1dbc1867fe084f27aae5c
Sherin Elsy Behanan <86961675+sherin-elsy-behanan@users.noreply.github.com>
2023-09-20 03:05:04 +0530

    Create pattern2.c

---
+++ Patterns/pattern2.c
@@ -0,0 +1,21 @@
+/*Pattern Given:
+*****
+*****
+*****
+*****
+*****
+*/
+#include <stdio.h>
+
+int main()
+{
+    int r=5;
+    for(int i=0;i<r;i++)
+    {
+    for(int j=0;j<r;j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    }
+}
